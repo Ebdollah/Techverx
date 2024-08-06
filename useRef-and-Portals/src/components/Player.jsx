c13a80fafd0c876378ac9bc5ea6dff62ebc94721
Ebdollah <l201165@lhr.nu.edu.pk>
2024-08-06 17:31:08 +0500

    implemented input without refs, so we need lot code to get input data, we need Ref.

--- useRef-and-Portals/src/components/Player.jsx
+++ useRef-and-Portals/src/components/Player.jsx
@@ -1,10 +1,21 @@
+import React,{useState} from "react";
+
 export default function Player() {
+  const [inputName, setinputName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const handleChange= (e) =>{
+    setSubmitted(false);
+    setinputName(e.target.value);
+  }
+  const handleSubmit = ()=>{
+    setSubmitted(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? inputName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" value={inputName} onChange={handleChange} />
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
