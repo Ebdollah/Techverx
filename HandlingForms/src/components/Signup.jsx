576cff6379c47a07f03958dea8edbdcff08e4db7
Ebdollah <l201165@lhr.nu.edu.pk>
2024-08-13 12:33:59 +0500

    outsourcing logic into resuable code

--- HandlingForms/src/components/Signup.jsx
+++ HandlingForms/src/components/Signup.jsx
@@ -1,111 +1,133 @@
-export default function Signup() {
-    function handleSubmit(event) {
-      event.preventDefault();
-  
-      const fd = new FormData(event.target);
-      const acquisitionChannel = fd.getAll('acquisition');
-      const data = Object.fromEntries(fd.entries());
-      console.log(data);
-      data.acquisition = acquisitionChannel;
-      console.log(data);
+import { useState } from 'react';
 
-    //   event.target.reset();
+export default function Signup() {
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const acquisitionChannel = fd.getAll('acquisition');
+    const data = Object.fromEntries(fd.entries());
+    data.acquisition = acquisitionChannel;
+
+    if (data.password !== data['confirm-password']) {
+      setPasswordsAreNotEqual(true);
+      return;
     }
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <h2>Welcome on board!</h2>
-        <p>We just need a little bit of data from you to get you started 🚀</p>
-  
-        <div className="control">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
-        </div>
-  
-        <div className="control-row">
-          <div className="control">
-            <label htmlFor="password">Password</label>
-            <input id="password" type="password" name="password" />
-          </div>
-  
-          <div className="control">
-            <label htmlFor="confirm-password">Confirm Password</label>
-            <input
-              id="confirm-password"
-              type="password"
-              name="confirm-password"
-            />
-          </div>
-        </div>
-  
-        <hr />
-  
-        <div className="control-row">
-          <div className="control">
-            <label htmlFor="first-name">First Name</label>
-            <input type="text" id="first-name" name="first-name" />
-          </div>
-  
-          <div className="control">
-            <label htmlFor="last-name">Last Name</label>
-            <input type="text" id="last-name" name="last-name" />
-          </div>
-        </div>
-  
-        <div className="control">
-          <label htmlFor="phone">What best describes your role?</label>
-          <select id="role" name="role">
-            <option value="student">Student</option>
-            <option value="teacher">Teacher</option>
-            <option value="employee">Employee</option>
-            <option value="founder">Founder</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
-  
-        <fieldset>
-          <legend>How did you find us?</legend>
-          <div className="control">
-            <input
-              type="checkbox"
-              id="google"
-              name="acquisition"
-              value="google"
-            />
-            <label htmlFor="google">Google</label>
-          </div>
-  
-          <div className="control">
-            <input
-              type="checkbox"
-              id="friend"
-              name="acquisition"
-              value="friend"
-            />
-            <label htmlFor="friend">Referred by friend</label>
-          </div>
-  
-          <div className="control">
-            <input type="checkbox" id="other" name="acquisition" value="other" />
-            <label htmlFor="other">Other</label>
-          </div>
-        </fieldset>
-  
-        <div className="control">
-          <label htmlFor="terms-and-conditions">
-            <input type="checkbox" id="terms-and-conditions" name="terms" />I
-            agree to the terms and conditions
-          </label>
-        </div>
-  
-        <p className="form-actions">
-          <button type="reset" className="button button-flat">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Sign up
-          </button>
-        </p>
-      </form>
-    );
+
+    console.log(data);
   }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Welcome on board!</h2>
+      <p>We just need a little bit of data from you to get you started 🚀</p>
+
+      <div className="control">
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" name="email" required />
+      </div>
+
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            required
+            minLength={6}
+          />
+        </div>
+
+        <div className="control">
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            id="confirm-password"
+            type="password"
+            name="confirm-password"
+            required
+          />
+          <div className="control-error">
+            {passwordsAreNotEqual && <p>Passwords must match.</p>}
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" name="first-name" required />
+        </div>
+
+        <div className="control">
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" name="last-name" required />
+        </div>
+      </div>
+
+      <div className="control">
+        <label htmlFor="phone">What best describes your role?</label>
+        <select id="role" name="role" required>
+          <option value="student">Student</option>
+          <option value="teacher">Teacher</option>
+          <option value="employee">Employee</option>
+          <option value="founder">Founder</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+
+      <fieldset>
+        <legend>How did you find us?</legend>
+        <div className="control">
+          <input
+            type="checkbox"
+            id="google"
+            name="acquisition"
+            value="google"
+          />
+          <label htmlFor="google">Google</label>
+        </div>
+
+        <div className="control">
+          <input
+            type="checkbox"
+            id="friend"
+            name="acquisition"
+            value="friend"
+          />
+          <label htmlFor="friend">Referred by friend</label>
+        </div>
+
+        <div className="control">
+          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <label htmlFor="other">Other</label>
+        </div>
+      </fieldset>
+
+      <div className="control">
+        <label htmlFor="terms-and-conditions">
+          <input
+            type="checkbox"
+            id="terms-and-conditions"
+            name="terms"
+            required
+          />
+          I agree to the terms and conditions
+        </label>
+      </div>
+
+      <p className="form-actions">
+        <button type="reset" className="button button-flat">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Sign up
+        </button>
+      </p>
+    </form>
+  );
+}
--- HandlingForms/src/components/StateLogin.jsx
+++ HandlingForms/src/components/StateLogin.jsx
@@ -1,84 +1,57 @@
-import { useState } from 'react';
+import Input from './Input.jsx';
+import { isEmail, isNotEmpty, hasMinLength } from '../util/validation.js';
+import { useInput } from '../hooks/useInput.js';
 
 export default function StateLogin() {
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [enteredPassword, setEnteredPassword] = useState('');
-  const [enteredValues, setEnteredValues] = useState({
-    email: '',
-    password: '',
-  });
-
-  const [didEdit, setDidEdit] = useState({
-    email: false,
-    password: false,
-  });
-
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+  const {
+    value: emailValue,
+    handleInputChange: handleEmailChange,
+    handleInputBlur: handleEmailBlur,
+    hasError: emailHasError,
+  } = useInput('', (value) => isEmail(value) && isNotEmpty(value));
+  const {
+    value: passwordValue,
+    handleInputChange: handlePasswordChange,
+    handleInputBlur: handlePasswordBlur,
+    hasError: passwordHasError,
+  } = useInput('', (value) => hasMinLength(value, 6));
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log(enteredValues);
+    if (emailHasError || passwordHasError) {
+      return;
+    }
+
+    console.log(emailValue, passwordValue);
   }
-
-  function handleInputChange(identifier, value) {
-    setEnteredValues((prevValues) => ({
-      ...prevValues,
-      [identifier]: value,
-    }));
-    setDidEdit((prevEdit) => ({
-      ...prevEdit,
-      [identifier]: false,
-    }));
-  }
-
-  function handleInputBlur(identifier) {
-    setDidEdit((prevEdit) => ({
-      ...prevEdit,
-      [identifier]: true,
-    }));
-  }
-
-  // function handleEmailChange(event) {
-  //   setEnteredEmail(event.target.value);
-  // }
-
-  // function handlePasswordChange(event) {
-  //   setEnteredPassword(event.target.value);
-  // }
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onBlur={() => handleInputBlur('email')}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-            value={enteredValues.email}
-          />
-          <div className="control-error">
-            {emailIsInvalid && <p>Please enter a valid email address.</p>}
-          </div>
-        </div>
+        <Input
+          label="Email"
+          id="email"
+          type="email"
+          name="email"
+          onBlur={handleEmailBlur}
+          onChange={handleEmailChange}
+          value={emailValue}
+          error={emailHasError && 'Please enter a valid email!'}
+        />
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-            value={enteredValues.password}
-          />
-        </div>
+        <Input
+          label="Password"
+          id="password"
+          type="password"
+          name="password"
+          onChange={handlePasswordChange}
+          onBlur={handlePasswordBlur}
+          value={passwordValue}
+          error={passwordHasError && 'Please enter a valid password!'}
+        />
       </div>
 
       <p className="form-actions">
