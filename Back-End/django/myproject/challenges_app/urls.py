2da10a11194ccf1088e3d3d33da0767adb900776
Ebdollah <l201165@lhr.nu.edu.pk>
2024-08-28 16:56:45 +0500

    dynamic view and rendering

--- Back-End/django/myproject/challenges_app/urls.py
+++ Back-End/django/myproject/challenges_app/urls.py
@@ -4,7 +4,10 @@ from django.shortcuts import HttpResponse
 from . import views
 
 urlpatterns = [
-    path('', lambda request : HttpResponse("Landing page"), name='ind'),
+    # path('', lambda request : HttpResponse("Landing page"), name='ind'),
+
+    path('challenges', views.month_list, name='month_challenge'),
+    path('', views.month_list, name='month_challenge'), #will work for /challenges
     path("<int:month>", views.monthly_challenges_by_number),
 
     path("<str:month>", views.monthly_challenge, name="month_challenge"),
--- Back-End/django/myproject/challenges_app/views.py
+++ Back-End/django/myproject/challenges_app/views.py
@@ -28,13 +28,23 @@ monthly_challenges = {
     "december" : "December month is aazingjwqdij qwuqw",
 }
 
+def month_list(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        month_path = reverse("month_challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{month}</a></li>"
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 def monthly_challenges_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
         return HttpResponseNotFound("Invalid month")
     redirect_month = months[month - 1]
     redirect_path = reverse("month_challenge", args=[redirect_month])
-    return HttpResponseRedirect(redirect_month)
+    return HttpResponseRedirect(redirect_path)
     # return HttpResponseRedirect("/challenges/" + redirect_month)
     # return HttpResponse(monthly_challenges[months[month]])
 
@@ -42,7 +52,7 @@ def monthly_challenges_by_number(request, month):
 def monthly_challenge(request, month):
     challenge_text = None
     if month in monthly_challenges.keys():
-        challenge_text = monthly_challenges[month]
+        challenge_text = f"<h1>{monthly_challenges[month]}</h1>"
     else:
         return "None"
     return HttpResponse(challenge_text)
