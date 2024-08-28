08f1d9bbc6f7efd650b1b1e09ca7b47edcfdb858
Ebdollah <l201165@lhr.nu.edu.pk>
2024-08-28 18:03:11 +0500

    added dynamic url

--- Back-End/django/myproject/myproject/urls.py
+++ Back-End/django/myproject/myproject/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('challenges_app.urls')),
+    # path('',include('challenges_app.urls')),
     path('challenges/' , include("challenges_app.urls")),
 ]
