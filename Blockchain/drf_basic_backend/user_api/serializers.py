8ba63fc8d05fe247ab6ea2f470eb68701c8b5957
Ebdollah <l201165@lhr.nu.edu.pk>
2024-09-30 13:23:15 +0500

    added user related featured

--- Blockchain/drf_basic_backend/user_api/serializers.py
+++ Blockchain/drf_basic_backend/user_api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model, authenticate
+from rest_framework.exceptions import ValidationError 
 
 UserModel = get_user_model()
 
@@ -8,20 +9,28 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 		model = UserModel
 		fields = '__all__'
 	def create(self, clean_data):
-		user_obj = UserModel.objects.create_user(email=clean_data['email'], password=clean_data['password'])
+		user_obj = UserModel.objects.create_user(
+            email=clean_data['email'],
+            username=clean_data['username'],  # Ensure the username is passed
+            password=clean_data['password']
+        )
 		user_obj.username = clean_data['username']
 		user_obj.save()
 		return user_obj
 
 class UserLoginSerializer(serializers.Serializer):
-	email = serializers.EmailField()
-	password = serializers.CharField()
-	##
-	def check_user(self, clean_data):
-		user = authenticate(username=clean_data['email'], password=clean_data['password'])
-		if not user:
-			raise ValidationError('user not found')
-		return user
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def check_user(self, clean_data):
+        admin_user = authenticate(username=clean_data['email'], password=clean_data['password'])
+        print(admin_user.is_superuser)  # Make sure indentation is consistent
+        if not admin_user:
+            raise ValidationError('User not found')
+        if not admin_user.is_superuser:
+            raise ValidationError('User is not admin')
+        return admin_user
+
 
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
