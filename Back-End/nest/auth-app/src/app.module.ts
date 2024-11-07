b94be3a711a0adf489a269e446684b0cdae635af
Ebdollah <l201165@lhr.nu.edu.pk>
2024-11-07 13:08:43 +0500

    modifying auth module completely

--- Back-End/nest/auth-app/src/app.module.ts
+++ Back-End/nest/auth-app/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
+
 
 @Module({
   imports: [
+    TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
