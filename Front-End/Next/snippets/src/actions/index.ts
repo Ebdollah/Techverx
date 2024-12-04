5377b54f3f41b71415fe081abbb4798872054d37
ebdolllah <abdullah.waseem@techverx.com>
2024-12-04 16:52:38 +0500

    added validations

--- Front-End/Next/snippets/src/actions/index.ts
+++ Front-End/Next/snippets/src/actions/index.ts
@@ -2,6 +2,39 @@
 import { db } from "@/db"
 import { redirect } from "next/navigation"
 
+
+export async function createSnippet(formState: {message: string}, formData: FormData) {
+    // This needs tobe a sever action!
+    // Check the user's inputs and make sure they're valid
+
+    try{
+    const title = formData.get('title') as string;
+    const code = formData.get('code') as string;
+
+    if(title.length < 0 || code.length < 0){
+        return { message : 'title or code is missing' }
+    }
+    // Create new record in the database
+    const snippet = await db.snippet.create({
+      data:{
+        title:title,
+        code:code
+      }
+    })
+    }catch (err: unknown) {
+        if (err instanceof Error) {
+        return {
+        message: err.message,
+        };
+    }else {
+        return {
+        message: 'Something went wrong....'
+        };
+    }
+    redirect('/');
+}
+}
+
 export async function editSnippet(id: number, code: string){
     await db.snippet.update({
         where:{id},
--- Front-End/Next/snippets/src/app/snippets/new/page.tsx
+++ Front-End/Next/snippets/src/app/snippets/new/page.tsx
@@ -1,25 +1,29 @@
+'use client'
+import { createSnippet } from "@/actions";
 import { db } from "@/db";
 import { redirect } from "next/navigation";
+import { useActionState, startTransition } from "react";
+
+
 
 export default function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    // This needs tobe a sever action!
-    "use server"
-    // Check the user's inputs and make sure they're valid
-    const title = formData.get('title') as string;
-    const code = formData.get('code') as string;
-    // Create new record in the database
-    const snippet = await db.snippet.create({
-      data:{
-        title:title,
-        code:code
-      }
-    })
+  
+  
 
-    redirect('/');
-  }
+    const [formState, action] : any = useActionState(createSnippet, {message: ''} )
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+      event.preventDefault();
+      const formData = new FormData(event.currentTarget);
+      startTransition(() => {
+        action(formData);
+      });
+    }
+  
+
+    // const createSnippetAction = createSnippet.bind(null)
+  
   return (
-    <form action={createSnippet}>
+    <form onSubmit={handleSubmit}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -43,6 +47,9 @@ export default function SnippetCreatePage() {
             id="code"
           />
         </div>
+        {
+          formState.message ? <div className="my-2 p-2 bg-red-200 border rounded border-red-400">{formState.message}</div> : null
+        }
 
         <button type="submit" className="rounded p-2 bg-blue-200">
           Create
